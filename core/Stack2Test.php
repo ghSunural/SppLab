8fba9aba07e86393ebbb01b92d43c696025c516a
ghSunural <ySunural@yandex.ru>
2020-02-01 22:06:43 +0500

    unit tests

---
+++ core/Stack2Test.php
@@ -0,0 +1,21 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class Stack2Test extends TestCase
+{
+    public function testPushAndPop()
+    {
+        $stack = [];
+        $this->assertSame(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[count($stack)-1]);
+        $this->assertSame(1, count($stack));
+
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertSame(0, count($stack));
+    }
+}
+
+?>
