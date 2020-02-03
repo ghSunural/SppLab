062055b0d98b9d3461928ba559f54ee8f9dace09
ghSunural <ySunural@yandex.ru>
2020-02-03 19:44:45 +0500

    remove lic

--- pages/calculators/scripts/geo/geocalcs.js
+++ pages/calculators/scripts/geo/geocalcs.js
@@ -5,7 +5,7 @@
 
         exports.alertshow = function () {
 
-            alert(kml.hot.getDataAtCell(1, 1));
+            alert(kml.hot.getDataAtCell(0, 0));
            // console.log(kml.hot.getDataAtCell(1, 1));
 
         };
